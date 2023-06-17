6e299ca3d8192a405d901f7534d9cb2b99f8364a
azizlibayim <bayimsha@code.edu.az>
2023-06-17 07:56:30 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString = "2023-06-17";
+            string format = "yyyy-MM-dd";
+            DateTime parsedDate;
+
+            if (TryParseExact(dateString, format, out parsedDate))
+            {
+                Console.WriteLine("Parsed Date: " + parsedDate.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static bool TryParseExact(string input, string format, out DateTime result)
+        {
+            return DateTime.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+        }
+
+    }
+}
